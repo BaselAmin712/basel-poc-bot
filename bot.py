c06573b9849cabccdbe6707ed1960d538e2e8547
BaselAmin712 <baselamin712@gmail.com>
2024-07-17 00:19:51 +0300

    /check_server_connection using the requests package

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import requests
+import os
+from dotenv import load_dotenv
 
-TOKEN = '7284725214:AAHrwfdFoSBTe6YQpN8rFLwt6Ou0yRYPEEU'
+load_dotenv()
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Hello! I am your bot. How can I help you?')
@@ -26,10 +30,15 @@ def get_details() -> str:
 async def details_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     details_text = get_details()
     await update.message.reply_text(details_text)
+    
+async def check_server_connection(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    response = requests.get(os.getenv('POKEMON_SERVICE_URL') + '/checkConnection').json()
+    # details_text = get_details()
+    await update.message.reply_text(response["connection"])
 
 def main():
     # Create the Application and pass it your bot's token.
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(os.getenv('TOKEN')).build()
 
     # Register the /start command handler
     application.add_handler(CommandHandler("start", start))
@@ -37,6 +46,8 @@ def main():
     application.add_handler(CommandHandler("help", help_command))
     # Register the /details command handler
     application.add_handler(CommandHandler("details", details_command))
+    # Register the /check server connection command handler
+    application.add_handler(CommandHandler("check_server_connection", check_server_connection))
 
     # Start the Bot
     application.run_polling()
